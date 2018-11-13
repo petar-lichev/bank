b56162aee7733b7668b2788c68db33958c88fd28
petar-lichev <p.lichev13@gmail.com>
2018-11-13 11:31:45 +0200

    Initial commit

---
+++ bg/src/main/java/com/bank/bg/BgApplication.java
@@ -0,0 +1,12 @@
+package com.bank.bg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgApplication.class, args);
+	}
+}
