cd029bb22be4c84c237e906463f1a597676a6097
petar-lichev <p.lichev13@gmail.com>
2018-11-14 12:11:37 +0200

    Account amount added and empty class TransactionService

--- bg/src/main/java/com/bank/bg/model/Account.java
+++ bg/src/main/java/com/bank/bg/model/Account.java
@@ -10,7 +10,17 @@ public class Account {
 	@Id
 	@GeneratedValue
 	private Long id;
+	
+	private Double amount;
 
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
 
 	public Long getId() {
 		return id;
---
+++ bg/src/main/java/com/bank/bg/service/TransactionService.java
@@ -0,0 +1,11 @@
+package com.bank.bg.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class TransactionService {
+
+}
